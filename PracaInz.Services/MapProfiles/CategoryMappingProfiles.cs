7727d0e2b12a2a2fc75da199299640fbdf317542
michal.deska <deska.michal96@gmail.com>
2021-05-12 21:17:44 +0200

    Przygotowanie automappera w Category

---
+++ PracaInz.Services/MapProfiles/CategoryMappingProfiles.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using PracaInz.ViewModels.CategoryViewModels;
+using PracaInz.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaInz.Services.MapProfiles
+{
+    public class CategoryMappingProfiles : Profile
+    {
+
+        public CategoryMappingProfiles()
+        {
+            // Due to the fact that all properties in both classess are the same, we don't have to create any special maps
+            CreateMap<Category, CategoryListItemViewModel>();
+            
+        }
+        
+    }
+}
